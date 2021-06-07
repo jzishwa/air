54ac7253b9f64487eaec6f387e2297bb03534bf6
ZISHWA MUH JAUHAR N(552083) <2215100701@mahasiswa.integra.its.ac.id>
2021-06-07 12:16:52 +0700

    Rubah file constant

--- app/core/Constans.php
+++ app/core/Constans.php
@@ -1,8 +1,8 @@
 <?php
 
-define('BASEURL', 'https://localhost/air');
+define('BASEURL', 'https://zishwa.com/air');
 date_default_timezone_set('Asia/Jakarta');
 define('URL', 'localhost');
-define('USER_DB', 'root');
-define('PASS_DB', '');
-define('NAMA_DB', 'pam');
+define('USER_DB', 'zishwaco_jauhar');
+define('PASS_DB', 'mawar100%');
+define('NAMA_DB', 'zishwaco_air');
